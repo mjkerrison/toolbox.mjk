b78fec6f14ad5f7264b44fe1495c949702eeacd2
mjkerrison <mjkerrison@users.noreply.github.com>
2024-02-04 13:47:52 +1100

    Split out compound inequalities

---
+++ R/compound_inequalities.R
@@ -0,0 +1,84 @@
+
+
+#' Infix operators for compound inequalities
+#'
+#' @description Kudos to http://jdobr.es/blog/compound-inequalities-r/! Not
+#'   exported. Lets you do things like x %<=% y %<=% z.
+#'
+#' @param lhs
+#' @param rhs
+#' @param comparison
+#'
+#' @return
+#'
+#' @examples
+compound.inequality <- function(lhs, rhs, comparison) {
+  if (is.null(attr(lhs, 'compound-inequality-partial'))) {
+    out <- rhs
+    attr(out, 'compound-inequality-partial') <- do.call(comparison, list(lhs, rhs))
+  } else {
+    out <- do.call(comparison, list(lhs, rhs)) & attr(lhs, 'compound-inequality-partial')
+  }
+  
+  return(out)
+}
+
+
+
+#' Strictly Less Than
+#'
+#' @param lhs 
+#' @param rhs 
+#'
+#' @return lhs < rhs ? TRUE : FALSE
+#' @export
+#'
+#' @examples
+'%<<%' <- function(lhs, rhs) {
+  return(compound.inequality(lhs, rhs, '<'))
+}
+
+
+
+#' Less Than Or Equal To
+#'
+#' @param lhs 
+#' @param rhs 
+#'
+#' @return lhs <= rhs ? TRUE : FALSE
+#' @export
+#'
+#' @examples
+'%<=%' <- function(lhs, rhs) {
+  return(compound.inequality(lhs, rhs, '<='))
+}
+
+
+
+#' Strictly Greater Than
+#'
+#' @param lhs 
+#' @param rhs 
+#'
+#' @return lhs > rhs ? TRUE : FALSE
+#' @export
+#'
+#' @examples
+'%>>%' <- function(lhs, rhs) {
+  return(compound.inequality(lhs, rhs, '>'))
+}
+
+
+
+#' Greater Than Or Equal To
+#'
+#' @param lhs 
+#' @param rhs 
+#'
+#' @return lhs >= rhs ? TRUE : FALSE
+#' @export
+#'
+#' @examples
+'%>=%' <- function(lhs, rhs) {
+  return(compound.inequality(lhs, rhs, '>='))
+}
--- R/utilities.R
+++ R/utilities.R
@@ -19,33 +19,6 @@ check_productivity <- function(){
 }
 
 
-# Thank you http://jdobr.es/blog/compound-inequalities-r/
-compound.inequality <- function(lhs, rhs, comparison) {
-  if (is.null(attr(lhs, 'compound-inequality-partial'))) {
-    out <- rhs
-    attr(out, 'compound-inequality-partial') <- do.call(comparison, list(lhs, rhs))
-  } else {
-    out <- do.call(comparison, list(lhs, rhs)) & attr(lhs, 'compound-inequality-partial')
-  }
-
-  return(out)
-}
-
-'%<<%' <- function(lhs, rhs) {
-  return(compound.inequality(lhs, rhs, '<'))
-}
-
-'%<=%' <- function(lhs, rhs) {
-  return(compound.inequality(lhs, rhs, '<='))
-}
-
-'%>>%' <- function(lhs, rhs) {
-  return(compound.inequality(lhs, rhs, '>'))
-}
-
-'%>=%' <- function(lhs, rhs) {
-  return(compound.inequality(lhs, rhs, '>='))
-}
 
 
 is_dttm <- function(tibble_col){
