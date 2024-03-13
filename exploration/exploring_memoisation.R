696307f24b169b29d308be354c568e2e79d424af
mjkerrison <mjkerrison@users.noreply.github.com>
2024-03-13 16:41:08 +1100

    Some very interesting options for bulk memoisation...

--- exploration/exploring_memoisation.R
+++ exploration/exploring_memoisation.R
@@ -1,6 +1,7 @@
 
 library(memoise)
 library(tidyverse)
+library(glue)
 
 test_fn <- function(...){
   
@@ -24,3 +25,59 @@ test_fn(5)
 # Ah-hah - so the caching *is* consistent across sessions...
 # But changing it does invalidate it!
 # Excellent...
+
+
+test_fn_2 <- function(...){
+  Sys.sleep(4)
+  return(68)
+}
+
+test_fn_3 <- function(...){
+  Sys.sleep(4)
+  return(70)
+}
+
+
+
+all_functions <- readLines("exploration/exploring_memoisation.R") |> 
+  
+  purrr::keep(
+    \(x) stringr::str_detect(x, " <- function\\(")
+  ) |> 
+  
+  stringr::str_extract(".+(?=<-)") |> 
+  
+  stringr::str_replace("\\s", "")
+
+
+
+for (fn_name in all_functions){
+  
+  fn_object <- get(fn_name)
+  
+  if(!memoise::is.memoised(get(fn_name))){
+    
+    print(glue("{fn_name} is not memoised"))
+    
+    assign(
+      fn_name,
+      value = memoise::memoise(
+        fn_object,
+        cache = cachem::cache_disk(dir = "temp")
+      )
+    )
+    
+  } else {
+    
+    print(glue("{fn_name} is already memoised"))
+    
+  }
+  
+}
+
+
+test_fn_2(5)
+
+test_fn_3(5)
+
+# Excellent - and this works with automating the memoisation!
